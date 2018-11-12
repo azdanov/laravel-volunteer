84ed9e442bc9cf2ec9a089a26cba9e05733a3ca5
Anton Zdanov <azdanov@pm.me>
2018-11-13 01:03:15 +0200

    chore(project): update dependencies

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-11-12 22:43:59.
+    * Generated on 2018-11-12 23:03:01.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.13 on 2018-11-12 22:43:58.
+ * Generated for Laravel 5.7.13 on 2018-11-12 23:03:01.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
