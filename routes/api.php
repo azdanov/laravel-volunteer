c690f722acc04c4403d0e959d63493bcc3c4f8f6
Anton Zdanov <azdanov@pm.me>
2018-11-20 19:56:20 +0200

    feat(routes): cleanup

--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,3 @@
 <?php
 
 declare(strict_types=1);
-
-use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', static function (Request $request) {
-    return $request->user();
-});
--- routes/channels.php
+++ routes/channels.php
@@ -1,18 +1,3 @@
 <?php
 
 declare(strict_types=1);
-
-/*
-|--------------------------------------------------------------------------
-| Broadcast Channels
-|--------------------------------------------------------------------------
-|
-| Here you may register all of the event broadcasting channels that your
-| application supports. The given channel authorization callbacks are
-| used to check if an authenticated user can listen to the channel.
-|
-*/
-
-Broadcast::channel('App.User.{id}', static function ($user, $id) {
-    return (int) $user->id === (int) $id;
-});
