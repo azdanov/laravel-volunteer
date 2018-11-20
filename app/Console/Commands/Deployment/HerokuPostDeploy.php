e08ef343e3ea480455df176c169637671dff043f
Anton Zdanov <azdanov@pm.me>
2018-11-20 18:26:40 +0200

    feat(commands): add postdeploy command

---
+++ app/Console/Commands/Deployment/HerokuPostDeploy.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands\Deployment;
+
+use Illuminate\Console\Command;
+
+class HerokuPostDeploy extends Command
+{
+    /** @var string */
+    protected $signature = 'postdeploy:heroku {--refresh : refresh database migrations.}';
+    /** @var string */
+    protected $description = 'Run post-deploy on heroku';
+
+    public function handle(): void
+    {
+        if (app()->environment('production')) {
+            $this->call('migrate', ['--force', true]);
+
+            return;
+        }
+
+        if ($this->option('refresh')) {
+            $this->call('migrate:refresh');
+        }
+
+        $this->call('migrate');
+
+        if ($this->option('seed')) {
+            $this->call('db:seed');
+        }
+    }
+}
