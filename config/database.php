ab5df238073c1095322200495351bd510bd67ecc
Anton Ždanov <azdanov@pm.me>
2019-02-18 22:07:36 +0200

    chore(database): use simpler sqlite defaults

--- config/database.php
+++ config/database.php
@@ -35,7 +35,7 @@ return [
     'connections' => [
         'sqlite' => [
             'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'database' => database_path('db.sqlite'),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
